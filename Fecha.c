90f5176ef246ab789d7f33abd174d40baaaf9357
Eneko <eneko.bazan@opendeusto.es>
2024-05-24 19:07:47 +0200

    fechas añadidas en c

---
+++ Fecha.c
@@ -0,0 +1,15 @@
+/*
+ * Fecha.c
+ *
+ *  Created on: 24 may 2024
+ *      Author: Eneko
+ */
+#include "fecha.h"
+#include <stdio.h>
+
+
+void imprimirFecha(Fecha f){
+	printf("%d/%d/%d\n", f.dia, f.mes, f.anyo);
+}
+
+
---
+++ Fecha.h
@@ -0,0 +1,19 @@
+/*
+ * Fecha.h
+ *
+ *  Created on: 24 may 2024
+ *      Author: Eneko
+ */
+
+#ifndef FECHA_H_
+#define FECHA_H_
+
+typedef struct{
+	int anyo;
+	int mes;
+	int dia;
+}Fecha;
+
+void imprimirFecha(Fecha f);
+
+#endif /* FECHA_H_ */
